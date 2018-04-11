c8a47fe44ca4ecbf98cccec166a5d22a2597b577
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2018-04-11 12:28:56 +0100

    Update osa_differ minimum version to 0.3.8
    In order to support the use of PR refspecs, we
    need to use 0.3.8 and above.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup
 required_packages = [
     "GitPython",
     "jinja2",
-    "osa_differ>=0.3.6",
+    "osa_differ>=0.3.8",
     "requests"
 ]
 
@@ -33,7 +33,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='rpc_differ',
-    version='0.3.6',
+    version='0.3.7',
     author='Major Hayden',
     author_email='major@mhtx.net',
     description="Find changes between RPC-OpenStack revisions",
